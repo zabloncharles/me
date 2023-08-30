b9fc2b0baf5077d635c4c729a1e40d0e32bd60fb
Zablon Charles <zablon.charles@gmail.com>
2023-08-29 23:35:17 -0500

    initial commit

---
+++ js/quiztyperdata.js
@@ -0,0 +1,19 @@
+var topicData = [
+    {
+      title: "WHAT IS AN AlGORITHM?",
+      num: "0",
+      desc: "In computer programming terms, an algorithm is a set of well-defined instructions to solve a particular problem. It takes a set of input(s) and produces the desired output.o",
+      image: "https://images.hdqwalls.com/wallpapers/boy-with-dog-o0.jpg",
+      lesson:
+        "An algorithm is a single, specific way of performing a complex task efficiently and repeatedly.",
+    },
+    {
+      title: "Data Structure and Types",
+      num: "1",
+      desc: "Data structure is a storage that is used to store and organize data. It is a way of arranging data on a computer so that it can be accessed and updated efficiently.",
+      image:
+        "https://static.vecteezy.com/system/resources/thumbnails/002/099/721/small/mountain-beautiful-landscape-background-design-illustration-free-vector.jpg",
+      lesson:
+        "In this article, you will learn about data structure and its types.What are Data Structures?.Data structure is a storage that is used to store and organize data. It is a way of arranging data on a computer so that it can be accessed and updated efficiently.Depending on your requirement and project, it is important to choose the right data structure for your project. For example, if you want to store data sequentially in the memory, then you can go for the Array data structure.Note: Data structure and data types are slightly different. Data structure is the collection of data types arranged in a specific order. Basically, data structures are divided into two categories:Linear data structure and Non-linear data structure.Let's learn about each type in detail.In linear data structures, the elements are arranged in sequence one after the other. Since elements are arranged in particular order, they are easy to implement.However, when the complexity of the program increases, the linear data structures might not be the best choice because of operational complexities.",
+    }
+  ];
